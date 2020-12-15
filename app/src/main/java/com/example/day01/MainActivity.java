2f8831ee6d466ce48b6b9dd82f4bc2583abecc82
wjc182 <782468817@qq.com>
2020-12-15 20:42:54 +0800

    第二次修改

--- app/src/main/java/com/example/day01/MainActivity.java
+++ app/src/main/java/com/example/day01/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.day01;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,5 +12,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         //dfvdfvfvfv
+        Toast.makeText(this, "混俊男真混蛋", Toast.LENGTH_SHORT).show();
     }
 }
