9e3418d8fb3362997af916c05f0b779bcd39eb88
wjc182 <782468817@qq.com>
2020-12-15 21:21:01 +0800

    大大改改

--- app/src/main/java/com/example/day01/MainActivity.java
+++ app/src/main/java/com/example/day01/MainActivity.java
@@ -3,7 +3,6 @@ package com.example.day01;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,7 +10,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //dfvdfvfvfv
-        Toast.makeText(this, "混俊男真混蛋", Toast.LENGTH_SHORT).show();
+        //1
     }
 }
--- app2/src/main/java/com/example/app2/MainActivity.java
+++ app2/src/main/java/com/example/app2/MainActivity.java
@@ -1,9 +1,8 @@
-package com.example.app1;
+package com.example.app2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,6 +10,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toast.makeText(this, "wve", Toast.LENGTH_SHORT).show();
+        //2
     }
 }
